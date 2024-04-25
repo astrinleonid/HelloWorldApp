f8a32546d6e08e99450782a7e3d1c12e0646580c
Leonid Astrin <astrinleonid@gmail.com>
2024-04-25 08:23:15 +0300

    Debugging

---
+++ app/src/main/java/com/example/helloworldapp/PlayRecordsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.helloworldapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlayRecordsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_play_records)
+    }
+}
